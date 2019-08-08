38ea626a6d24127fc05ebf27e120be3343860148
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-08 21:58:55 +0200

    Add basic test page

---
+++ src/components/views/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home-page">
+    Hello world
+
+    <el-date-picker
+      v-model="value1"
+      type="date"
+      placeholder="Pick a day">
+    </el-date-picker>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-page',
+  data() {
+    return {
+      value1: null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.nahome-pageme {}
+</style>
