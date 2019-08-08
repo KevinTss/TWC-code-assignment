2fdc95fd69e357e5300409fc6695d5c5b91d8621
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-08 22:37:58 +0200

    Add MainHeader component

---
+++ src/components/ui/layout/MainHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-header class="main-header">
+
+    <el-row>
+
+      <el-col :span="6">
+        <el-image
+          class="logo"
+          :src="logoUrl"
+          fit="contain"
+        ></el-image>
+      </el-col>
+
+      <el-col :span="18">
+        <!-- nav -->
+      </el-col>
+
+    </el-row>
+
+  </el-header>
+</template>
+
+<script>
+import logo from '../../../assets/images/openweather.png'
+
+export default {
+  name: 'main-header',
+  computed: {
+    logoUrl: () => logo
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../../assets/styles/index";
+
+.main-header {
+  background-color: $background-dark;
+
+  .el-row {
+    .el-col {
+      display: flex;
+      align-items: center;
+      height: 60px;
+
+      .logo { height: 45px }
+    }
+  }
+}
+</style>
