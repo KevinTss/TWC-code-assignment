08a6b45881741cb44a5a8e55277124d3e33e0134
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-09 17:56:50 +0200

    Prepare getters for weather data

---
+++ src/store/modules/weather/getters.js
@@ -0,0 +1,47 @@
+import { GET_CURRENT_WEATHER } from './constants'
+
+export default {
+
+  /**
+   * weatherCityList
+   * @return {Array} - List of weather cities.
+   */
+  weatherCityList: state => Object.keys(state.list),
+
+  /**
+   * weatherDataList
+   * @return {Array} - List of weather data.
+   */
+  weatherDataList: state => Object.values(state.list),
+
+  /**
+   * isWeatherLoading
+   * @return {Boolean} - If offer is loading.
+   */
+  isWeatherLoading: state => state.actions[GET_CURRENT_WEATHER].loading,
+
+  /**
+   * hasWeatherError
+   * @return {String|Null} - If weather has error.
+   */
+  hasWeatherError: state => state.actions[GET_CURRENT_WEATHER].error,
+
+  /**
+   * isWeatherByCityLoading
+   * @param {String} city
+   * @return {Function} - This function return if a specific city weather is loading.
+   */
+  isWeatherByCityLoading(state) {
+    return city => state.list[city] && state.list[city] === 'loading'
+  },
+
+  /**
+   * weatherByCity
+   * @param {String} city
+   * @return {Function} - This function return a specific city weather.
+   */
+  weatherByCity(state) {
+    return city => state.list[city]
+  },
+
+}
