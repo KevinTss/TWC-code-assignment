07861696a9c63367f74a947d8b749c081a7baca2
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-09 17:57:48 +0200

    Add weather module in main store

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
 
-// import weather from './modules/weather'
+import weather from './modules/weather'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   strict: process.env.NODE_ENV === 'development',
   plugins: process.env.NODE_ENV === 'development' ? [ createLogger() ] : [],
-  // modules: { weather }
+  modules: { weather }
 })
