5d962c2d632d9f112ed533079f187a5500356941
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-09 17:56:21 +0200

    Set global constants

---
+++ src/_helpers/globalConstants.js
@@ -0,0 +1,5 @@
+export const REQUEST = 'REQUEST__'
+export const FAILED = 'FAILED__'
+export const SUCCESS = 'SUCCESS__'
+export const OPEN_WEATHER_KEY = process.env.VUE_APP_OPEN_WEATHER_KEY
+export const WEATHER_API_DOMAIN = 'https://samples.openweathermap.org/data/2.5/'
