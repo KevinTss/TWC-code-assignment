e5889558bb707fa80b6592ffa7a6605fc49170c0
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-09 18:07:46 +0200

    Fix naming constant

--- src/_helpers/globalConstants.js
+++ src/_helpers/globalConstants.js
@@ -2,4 +2,4 @@ export const REQUEST = 'REQUEST__'
 export const FAILED = 'FAILED__'
 export const SUCCESS = 'SUCCESS__'
 export const OPEN_WEATHER_KEY = process.env.VUE_APP_OPEN_WEATHER_KEY
-export const WEATHER_API_DOMAIN = 'https://samples.openweathermap.org/data/2.5/'
+export const WEATHER_API_BASE_URL = 'https://samples.openweathermap.org/data/2.5/'
