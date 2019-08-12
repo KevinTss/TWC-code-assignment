fd7b030fef9464f1a93668eb9a00e8b6936c7813
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-13 01:39:43 +0200

    Create the service for weather

---
+++ src/store/modules/weather/service.js
@@ -0,0 +1,16 @@
+import HttpClient from '../../../_helpers/HttpClient'
+import { OPEN_WEATHER_KEY, WEATHER_API_BASE_URL } from '../../../_helpers/globalConstants'
+
+const API = new HttpClient(WEATHER_API_BASE_URL)
+
+export default {
+
+  /**
+   * getWeatherByCity
+   * @param {String} city
+   */
+  getWeatherByCity(city) {
+    return API.get(`weather?q=${city}&appid=${OPEN_WEATHER_KEY}`)
+  },
+
+}
