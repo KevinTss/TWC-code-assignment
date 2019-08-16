18d8819a4e067144a02cea8764dab1fba82c8e0e
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-16 09:42:56 +0200

    Expose setting store module

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
 
+import setting from './modules/setting'
 import weather from './modules/weather'
 import city from './modules/city'
 
@@ -11,6 +12,7 @@ export default new Vuex.Store({
   strict: process.env.NODE_ENV === 'development',
   plugins: process.env.NODE_ENV === 'development' ? [ createLogger() ] : [],
   modules: {
+    setting,
     weather,
     city
   }
