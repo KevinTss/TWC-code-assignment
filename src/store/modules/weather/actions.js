0d8a7d05ac8125e0ea65546c5644e076a3f7252e
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-16 09:43:30 +0200

    Add weather action

---
+++ src/store/modules/weather/actions.js
@@ -0,0 +1,27 @@
+import { REQUEST, FAILED, SUCCESS } from '../../../_helpers/globalConstants'
+import { GET_CURRENT_WEATHER } from './constants'
+import service from './service'
+
+export default {
+
+  /**
+   * getCurrentWeather
+   *
+   * Method who fetch the current weather base on a city
+   *
+   * @param {Object} Context
+   * @param {Object} city
+   * @return {Void}
+   */
+  async getCurrentWeather({ commit }, city) {
+    commit(`${REQUEST}${GET_CURRENT_WEATHER}`)
+    try {
+      const weatherData = await service.getWeatherByCity(city)
+      commit('set_weather_by_city', { city, weatherData })
+      commit(`${SUCCESS}${GET_CURRENT_WEATHER}`)
+    } catch (error) {
+      commit(`${FAILED}${GET_CURRENT_WEATHER}`, error.message)
+    }
+  }
+
+}
