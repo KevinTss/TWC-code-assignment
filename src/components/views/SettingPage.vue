e8a73a4d370b99e70dd5ca70cb3654a03682be72
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-16 09:43:16 +0200

    Design the setting page

--- src/components/views/SettingPage.vue
+++ src/components/views/SettingPage.vue
@@ -7,20 +7,56 @@
       label-position="top">
 
       <el-form-item
-        prop="email"
-        label="Email">
+        prop="temperature"
+        label="Temparature">
         <el-slider
           v-model="formData.temparatures"
-          range>
+          :min="-20"
+          :max="50"
+          show-input-controls
+          range
+          vertical
+          height="300px"
+          input-size="large">
         </el-slider>
       </el-form-item>
 
       <el-form-item
-        prop="email"
-        label="Email">
-        <el-checkbox-group v-model="checkboxGroup1">
-          <el-checkbox-button v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox-button>
+        prop="conditions"
+        label="Weather conditions">
+        <el-checkbox-group v-model="formData.conditions">
+          <el-checkbox-button
+            v-for="item in conditionOptions"
+            :label="item.label"
+            :key="item.label">
+            {{ item.label }}
+          </el-checkbox-button>
         </el-checkbox-group>
+      </el-form-item>
+
+      <el-form-item
+        prop="wind"
+        label="Wind">
+        <el-slider
+          v-model="formData.windSpeed"
+          show-input-controls
+          range
+          input-size="large">
+        </el-slider>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button
+          type="text"
+          icon="el-icon-back"
+          @click="back">
+          Back
+        </el-button>
+        <el-button
+          type="primary"
+          @click="savePreferences">
+          Save
+        </el-button>
       </el-form-item>
 
     </el-form>
@@ -29,38 +65,53 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapState, mapMutations } from 'vuex'
 
 export default {
   name: 'settings-page',
   data() {
     return {
-      formData: {
-        temparatures: [-10, 20]
-      }
+      formData: {},
+      conditionOptions: [
+        { label: 'Thunderstorm' },
+        { label: 'Drizzle' },
+        { label: 'Rain' },
+        { label: 'Snow' },
+        { label: 'Clear' },
+        { label: 'Clouds' },
+      ]
     }
   },
   computed: {
-    ...mapGetters(['citiesList', 'isCitiesAreLoading']),
+    ...mapState(['setting']),
   },
   methods: {
-    ...mapActions(['save']),
+    ...mapMutations(['set_preferences']),
     savePreferences() {
-      this.$refs[formName].validate((valid) => {
+      this.$refs['preference'].validate(valid => {
         if (valid) {
-          alert('submit!');
+          this.set_preferences(this.formData)
+          this.$message({
+            message: 'Preferences saved',
+            type: 'success'
+          })
         } else {
-          console.log('error submit!!');
-          return false;
+          this.$message({
+            message: 'Something wrong happened, try again',
+            type: 'danger'
+          })
         }
       })
     },
+    back() {
+      this.$router.push({ name: 'home' })
+    }
   },
   watch: {
 
   },
   created() {
-
+    this.formData = { ...this.setting.preferences }
   }
 }
 </script>
