6fa1fb3e43b294bd4a217c61455c7f36cb224ea0
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-16 09:43:45 +0200

    Add links to the navigation

--- src/components/ui/layout/MainHeader.vue
+++ src/components/ui/layout/MainHeader.vue
@@ -8,11 +8,16 @@
           class="logo"
           :src="logoUrl"
           fit="contain"
+          @click="goToHome"
         ></el-image>
       </el-col>
 
-      <el-col :span="18">
-        <!-- nav -->
+      <el-col :span="18" class="menu">
+        <el-button
+          icon="el-icon-setting"
+          type="primary"
+          @click="goToSettings">
+        </el-button>
       </el-col>
 
     </el-row>
@@ -28,6 +33,14 @@ export default {
   computed: {
     logoUrl: () => logo
   },
+  methods: {
+    goToHome() {
+      this.$router.push({ name: 'home' })
+    },
+    goToSettings() {
+      this.$router.push({ name: 'setting' })
+    }
+  }
 }
 </script>
 
@@ -43,7 +56,15 @@ export default {
       align-items: center;
       height: 60px;
 
-      .logo { height: 45px }
+      &.menu {
+        justify-content: flex-end;
+        padding-right: 20px;
+      }
+
+      .logo {
+        height: 45px;
+        cursor: pointer;
+      }
     }
   }
 }
