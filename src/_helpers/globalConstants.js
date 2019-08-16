0053008957cbfd8206f8fe7545f29d704faa484c
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-16 10:33:19 +0200

    Add constant function to get icon URL

--- src/_helpers/globalConstants.js
+++ src/_helpers/globalConstants.js
@@ -3,3 +3,4 @@ export const FAILED = 'FAILED__'
 export const SUCCESS = 'SUCCESS__'
 export const OPEN_WEATHER_KEY = process.env.VUE_APP_OPEN_WEATHER_KEY
 export const WEATHER_API_BASE_URL = 'http://api.openweathermap.org/data/2.5/'
+export const GET_WEATHER_ICON_URL = iconName => `http://openweathermap.org/img/wn/${iconName}@2x.png`
